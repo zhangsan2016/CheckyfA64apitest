08b58211cda8ef3c9205e0dda220c645cb36eccf
liu <602694077@qq.com>
2020-12-21 15:31:54 +0800

    2020年12月21日 15:31:48 测试版改回常规正常版本，人脸识别ftp服务器正常，修改了francis帐号权限，修复ftp内存中的配置文件被删除从新启动时bug  -- 完成

--- app/src/main/java/smartcity/ldgd/com/checkyfa64apitest/activity/MainActivity.java
+++ app/src/main/java/smartcity/ldgd/com/checkyfa64apitest/activity/MainActivity.java
@@ -559,7 +559,6 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
 
     private void startFtpService() {
         String config = getString(R.string.users);
-        System.out.println("xxxxxxxxxxxxx " + config);
         FtpManager ftpManager = FtpManager.getInstance(config);
         ftpManager.startServer();
     }
